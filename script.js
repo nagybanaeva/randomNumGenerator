fe82b5ccdf5eb5d7fa4dc4ac66e9cce4a8c444fc
Nagy-Bana Éva <nagybanaeva@gmail.com>
2019-12-20 15:51:23 +0100

    add fixes

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var x = Math.floor(Math.random()*100)+1;
-document.write('Gondoltam egy számra 1 és 100 között! <br />');
+document.write(document.getElementById('intro'));
 
 do{
 	var tipp = prompt('Add meg a tipped!');
 			if(tipp>x) {
-				document.write('Kisebbet! <br />');
+				alert('Kisebbet! <br />');
 			}
 			else if(tipp<x) {
-				document.write('Nagyobbat! <br />');
+				alert('Nagyobbat! <br />');
 			}
 			}
 			while (tipp!=x);
-				document.write('Gratulálok, a gondolt szám: ', x, 'volt!');
+				alert('Gratulálok, a gondolt szám: ' + x);
